25079f1859f6580b22a5429685d53c6676320bf9
Kirill Milinevskiy <k.milinevskiy@tinkoff.ru>
2020-02-07 17:17:32 +0300

    dtos

--- src/main/kotlin/ru/kekreal/kobabot/handler/MessageHandler.kt
+++ src/main/kotlin/ru/kekreal/kobabot/handler/MessageHandler.kt
@@ -1,5 +1,6 @@
 package ru.kekreal.kobabot.handler
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -10,6 +11,56 @@ class MessageHandler {
 
     fun process(m: ServerRequest): Mono<out ServerResponse> {
         println("hello I'm kobabot")
+        println(m.bodyToFlux(Update::class.java))
         return Mono.empty()
     }
 }
+
+data class Update(
+    @JsonProperty("update_id")
+    val updateId: String,
+    val message: Message
+)
+
+data class Message(
+    @JsonProperty("message_id")
+    val id: Long,
+    @JsonProperty("from")
+    val user: TelegramUser?,
+    @JsonProperty("date")
+    val date: Long,
+    @JsonProperty("chat")
+    val chat: TelegramChat,
+    @JsonProperty("text")
+    val text: String?
+)
+
+data class TelegramUser(
+    @JsonProperty("id")
+    val id: Long,
+    @JsonProperty("first_name")
+    val firstName: String,
+    @JsonProperty("last_name")
+    val lastName: String?,
+    @JsonProperty("username")
+    val username: String?
+)
+
+data class TelegramChat(
+
+    @JsonProperty("id")
+    val id: Long,
+    /**
+     * PRIVATE, GROUP, SUPERGROUP, CHANNEL
+     */
+    @JsonProperty("type")
+    val type: String,
+    @JsonProperty("title")
+    val title: String?,
+    @JsonProperty("username")
+    val username: String?,
+    @JsonProperty("first_name")
+    val firstName: String?,
+    @JsonProperty("last_name")
+    val lastName: String?
+)
